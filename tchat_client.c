85cfdcc6f6e3a385e913deae8c197b76235640ee
Nolan Brown <njbrown302@gmail.com>
2024-07-19 15:50:09 -0500

    added pthreads to client with read and write

--- tchat_client.c
+++ tchat_client.c
@@ -1,24 +1,80 @@
 #include <stdio.h>
 #include <string.h>
-
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <unistd.h>
+#include <pthread.h>
 
 #define PORT 8080
+#define BUFFER_SIZE 256
+
+void *read_thread(void *ptr);
+void *write_thread(void *ptr);
+
+int flag;
 
 int main(int argc, char **argv)
 {
-    int clientfd;
-    clientfd = socket(AF_INET, SOCK_STREAM, 0);
+    int sockfd;
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    printf("Client: 1/2\n");
 
     struct sockaddr_in serv_addr;
+    serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(PORT);
 
-    connect(clientfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+    connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
 
-    close(clientfd);
+    printf("Client: 2/2\n");
+
+    flag = 1;
+
+    pthread_t read, write;
+    pthread_create(&read, NULL, read_thread, &sockfd);
+    pthread_create(&write, NULL, write_thread, &sockfd);
+
+    pthread_join(read, NULL);
+    pthread_join(write, NULL);
+
+    close(sockfd);
 
     return 0;
+}
+
+void *read_thread(void *ptr)
+{
+    int sockfd = *(int *)ptr;
+
+    char read_buffer[BUFFER_SIZE];
+
+    while (flag)
+    {
+        read(sockfd, read_buffer, BUFFER_SIZE);
+        printf("Client: read message from server\n");
+        printf("%s\n", read_buffer);
+    }
+
+    return NULL;
+}
+
+void *write_thread(void *ptr)
+{
+    int sockfd = *(int *)ptr;
+
+    char write_buffer[BUFFER_SIZE];
+
+    while (flag)
+    {
+        fgets(write_buffer, BUFFER_SIZE, stdin);
+        write(sockfd, write_buffer, BUFFER_SIZE);
+        printf("Client: write to server\n");
+        if (write_buffer[0] == '/' && write_buffer[1] == 'q')
+        {
+            flag = 0;
+        }
+    }
+
+    return NULL;
 }
--- tchat_server.c
+++ tchat_server.c
@@ -12,19 +12,23 @@
 
 char write_buffer[BUFFER_SIZE];
 
-pthread_mutex_t *mutex;
+pthread_mutex_t mutex;
 
 int client_list[MAX_CLIENT];
 int next_client;
 
 void *thread(void *ptr);
-void send();
+void send_buffer();
 void find_next_client();
+void remove_client(int clientfd);
+int check_command(int clientfd, char *read_buffer);
 
 int main(int argc, char **argv)
 {
     int serverfd;
     serverfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    setsockopt(serverfd, SOL_SOCKET, SO_REUSEADDR, &(int){1}, sizeof(int));
 
     struct sockaddr_in address;
     address.sin_family = AF_INET;
@@ -38,7 +42,7 @@ int main(int argc, char **argv)
     int clientfd;
     socklen_t addrlen = sizeof(address);
 
-    pthread_mutex_init(mutex, NULL);
+    pthread_mutex_init(&mutex, NULL);
 
     for (int i = 0; i < MAX_CLIENT; i++)
     {
@@ -46,25 +50,31 @@ int main(int argc, char **argv)
     }
     next_client = 0;
 
+    pthread_t client_thread;
+
+    printf("Server: setup done\n");
+
     while (1)
     {
         clientfd = accept(serverfd, (struct sockaddr *)&address, &addrlen);
+
+        printf("Server: new client %d\n", clientfd);
 
         if (next_client == MAX_CLIENT)
         {
             printf("Max number of clients reached\n");
             close(clientfd);
+            find_next_client();
         }
         else
         {
-            printf("New client: %d\n", clientfd);
             client_list[next_client] = clientfd;
-            find_next_client();
+            pthread_create(&client_thread, NULL, thread, &clientfd);
+            pthread_detach(client_thread); // let the thread terminate when client disconnects
+            // find_next_client();
+            next_client += 1;
+            printf("Server: next client index %d\n", next_client);
         }
-
-        pthread_t *client_thread;
-        pthread_create(client_thread, NULL, thread, &clientfd);
-        pthread_detach(*client_thread); // let the thread terminate when client disconnects
     }
 
     close(serverfd);
@@ -76,28 +86,49 @@ void *thread(void *ptr)
 {
     int clientfd = *(int *)ptr;
 
-    char *read_buffer = malloc(BUFFER_SIZE);
-    read(clientfd, read_buffer, BUFFER_SIZE);
+    char read_buffer[BUFFER_SIZE];
 
-    pthread_mutex_lock(mutex);
+    while (1)
+    {
+        read(clientfd, read_buffer, BUFFER_SIZE);
 
-    memcpy(write_buffer, read_buffer, BUFFER_SIZE);
+        printf("Server: read message from %d\n", clientfd);
 
-    // send to all clients
+        if (read_buffer[0] == '/')
+        {
+            if (check_command(clientfd, read_buffer))
+            {
+                remove_client(clientfd);
+                close(clientfd);
+                return NULL;
+            }
+            else
+            {
+                continue;
+            }
+        }
 
-    pthread_mutex_unlock(mutex);
+        pthread_mutex_lock(&mutex);
+
+        memcpy(write_buffer, read_buffer, BUFFER_SIZE);
+
+        send_buffer();
+
+        pthread_mutex_unlock(&mutex);
+    }
 
     close(clientfd); // for now
 
     return NULL;
 }
 
-void send() // send buffer to all clients
+void send_buffer() // send buffer to all clients
 {
     for (int i = 0; i < MAX_CLIENT; i++)
     {
         if (client_list[i] != -1)
         {
+            printf("Server: sent message to %d\n", client_list[i]);
             write(client_list[i], write_buffer, BUFFER_SIZE);
         }
     }
@@ -113,10 +144,28 @@ void find_next_client()
             return;
         }
     }
+
+    next_client == MAX_CLIENT;
 }
 
-/*
-TODO:
-    remove client function
-    discover disconnected clients and manual disconnects (client commands?)
-*/
+void remove_client(int clientfd)
+{
+    for (int i = 0; i < MAX_CLIENT; i++)
+    {
+        if (client_list[i] = clientfd)
+        {
+            client_list[i] = -1;
+            return;
+        }
+    }
+}
+
+int check_command(int clientfd, char *read_buffer)
+{
+    if (read_buffer[1] == 'q')
+    {
+        return 1;
+    }
+
+    return 0;
+}
